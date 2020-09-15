2f91c4b55d8d050d95e527436085d49f9b553eba
Miguel Saravia <mikisaraviaios@gmail.com>
2020-09-15 11:49:55 -0400

    login app done and config textfield and boton

---
+++ chatOnline/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,69 @@
+//
+//  LoginViewController.swift
+//  chatOnline
+//
+//  Created by Miguel Angel Saravia Belmonte on 9/15/20.
+//  Copyright © 2020 chatOnline. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    // outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configOutlets()
+    }
+    
+    func configOutlets() {
+        // config emailsTextfield
+        emailTextField.autocapitalizationType = .none
+        emailTextField.autocorrectionType = .no
+        emailTextField.returnKeyType = .continue
+        emailTextField.placeholder = "Email Address..."
+        emailTextField.leftViewMode = .always
+        emailTextField.backgroundColor = .secondarySystemBackground
+        // config passwordTextfield
+        passwordTextField.autocapitalizationType = .none
+        passwordTextField.autocorrectionType = .no
+        passwordTextField.returnKeyType = .done
+        passwordTextField.placeholder = "Password..."
+        passwordTextField.leftViewMode = .always
+        passwordTextField.backgroundColor = .secondarySystemBackground
+        passwordTextField.isSecureTextEntry = true
+        // button login
+        loginButton.setTitle("Log In", for: .normal)
+        loginButton.backgroundColor = .link
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.layer.cornerRadius = 12
+        loginButton.layer.masksToBounds = true
+        loginButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+    }
+    
+    
+    @IBAction func LoginAction(_ sender: Any) {
+    }
+    
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    func hidekeyboardWhentappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+}
