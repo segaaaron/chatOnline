e82d7bbfdcbbc9ed44d7bfa28be05061e92a5d93
Miguel Saravia <mikisaraviaios@gmail.com>
2020-09-15 10:28:00 -0400

    Initial Commit

---
+++ chatOnline/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chatOnline
+//
+//  Created by Miguel Angel Saravia Belmonte on 9/15/20.
+//  Copyright © 2020 chatOnline. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
